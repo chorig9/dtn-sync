d788d39af6e74f9382c8486f19dcc92c2bc2fa1f
Igor Chorazewicz <chorig9@gmail.com>
2019-08-11 16:49:11 +0200

    Implement rdiff support and synchronization
    Done:
    - fast-forward synchronization
    - out-of-order patches
    
    Todo:
    - conflict resolution

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 import argparse
 import synchronization
-import monitor
-
-from conflict_resolution import resolve_conflict 
+import logging
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", help="Port on which dtn-sync operates", type=int, required=True)
 parser.add_argument("-d", "--directory", help="Directory which will be synchronized - it must exist", required=True)
+parser.add_argument("-l", "--log", help="Logging level", required=False)
 
 args = parser.parse_args()
 
-sync = synchronization.SyncWorker(args.directory, args.port, resolve_conflict)
-monitor = monitor.Monitor(args.directory, sync)
+# Set logging level
+if args.log:
+    numeric_level = getattr(logging, args.log.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % args.log)
+    logging.basicConfig(level=numeric_level)
+
+sync = synchronization.SyncWorker(args.directory, args.port)
