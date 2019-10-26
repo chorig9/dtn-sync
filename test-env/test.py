7d04a568d3791b43806c23cf2aa0399599590238
Igor Chorazewicz <chorig9@gmail.com>
2019-10-26 16:42:20 +0200

    Add test for synchronization

--- test-env/test.py
+++ test-env/test.py
@@ -10,7 +10,25 @@ class BasicTests(AbstractTest):
 
 		self.env.finish()
 
+class OutOfOrderPatchTests(AbstractTest):
+	TEST_PORT = "1234"
+	DTN_SOURCE_DIR = "/home/igor/Projects/dtn-sync/src/main.py"
+
+	def test1(self):
+		self.env.run_icommand(0, ["python3", OutOfOrderPatchTests.DTN_SOURCE_DIR, "-p", OutOfOrderPatchTests.TEST_PORT, "-d", "/tmp/test_node0"])
+		self.env.run_icommand(1, ["python3", OutOfOrderPatchTests.DTN_SOURCE_DIR, "-p", OutOfOrderPatchTests.TEST_PORT, "-d", "/tmp/test_node1"])
+
+		self.env.run_icommand(2, ["echo", "TEST1", ">", "/tmp/test_node0/file1"])
+
+		self.env.run_icommand(2, ["cat", "/tmp/test_node0/file1"])
+		self.env.run_icommand(2, ["cat", "/tmp/test_node1/file1"])
+
+		self.env.finish()
+
 
 if __name__ == "__main__":
-	basic_tests = BasicTests("c3.json")
-	basic_tests.test1()
+	#basic_tests = BasicTests("c3.json")
+	#basic_tests.test1()
+
+	out_of_order_patch_tests = OutOfOrderPatchTests("c3.json")
+	out_of_order_patch_tests.test1()
